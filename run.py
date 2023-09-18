d434445c8980bd6d1ac4b91084d3b09463b5f7d0
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-18 11:10:33 +0200

    Add story function to call openAI and get reply

--- run.py
+++ run.py
@@ -440,6 +440,30 @@ def sword_battle(player, enemy_lst, enemy, num):
                 break
 
 
+def story(player, enemy):
+    """
+    Api call to chat-gpt asking it to reply to a string prepared with type and name.
+    Length limit of the reply is included in the string
+    """
+    clear_screen()
+    messages = [
+        {"role": "system", "content": "You are a Storyteller"},
+    ]
+    message = f"""Set up with dialouge that leads to {player.name} the {player.char_type}
+               and {enemy.name} the {enemy.char_type} drawing their weapons and comencing
+               a sword_battle against eachother. Maximum length 70 words"""
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    reply = chat.choices[0].message.content
+    print(f"\t\t⚔⚔⚔---LORD OF THE STRINGS---⚔⚔⚔\n\n{reply}")
+    messages.append({"role": "assistant", "content": reply})
+    start_battle = input("\nPress Enter to start the battle")
+
+
 def main():
     player = "Player has not been created"
     clear_screen()
