5d9aace717905016875e84154a26f0c213223888
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-18 09:53:24 +0200

    Add class to organize player and enemy stats

--- run.py
+++ run.py
@@ -14,8 +14,48 @@ SHEET = GSPREAD_CLIENT.open("enemy").sheet1
 MOREENEMIES = GSPREAD_CLIENT.open("reset").sheet1
 
 
+def text_center(text):
+    """
+    Placing text in center of 62 Character line
+    """
+    txt = f"{text}"
+    center_txt = txt.center(62)
+    print(center_txt)
+
+
 def game_title():
     """
     Prints the game title
     """
     text_center("⚔⚔⚔---LORD OF THE STRINGS---⚔⚔⚔")
+
+
+class CharacterStats:
+    """
+    Object collects the character and selected enemy stats. __str__ used to make
+    the print of the object prettier.
+    """
+
+    def __init__(
+        self, char_type, name, strength_points, health_points, skill_points, armor
+    ):
+        self.char_type = char_type
+        self.name = name
+        self.strength_points = strength_points
+        self.health_points = health_points
+        self.skill_points = skill_points
+        self.armor = armor
+
+    def __str__(self):
+        return f"""{self.name.upper()} THE MIGHTY {self.char_type.upper()}\n
+        STRENGTH:\t{self.strength_points}\n
+        HEALTH:\t\t{self.health_points}\n
+        SWORD SKILL:\t{self.skill_points}\n
+        ARMOR:\t\t{self.armor}"""
+
+
+def main():
+    game_title()
+
+
+main()
