1e9a9205ba82bd89939072352a3d250227f234d9
Johan Plym Arkert <johan.arkert@gmail.com>
2023-09-18 10:42:27 +0200

    Add battle_dice to increase game logic unceartanty

--- run.py
+++ run.py
@@ -53,6 +53,27 @@ def dice(num):
     return total
 
 
+def battle_dice(num, total):
+    """
+    Simulates a six sided dice with the addition that all sixes generates two new
+    rolls of dice. This function is only used in the sword_battle function to
+    increase serendipity and allow higher uncertanty in the battles.
+    """
+    result = 0
+    sixes = []
+    for _ in range(num):
+        result = random.randint(1, 6)
+        total += result
+        if result != 6:
+            result += total
+            num -= 1
+        else:
+            sixes.append(result)
+    if len(sixes) > 0:
+        battle_dice(num, total)
+    return total
+
+
 def read_enemy_csv():
     """
     Reads the google sheet file and storing it in the variable enemy_lst.
